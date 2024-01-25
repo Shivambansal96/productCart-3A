7225fee69bda3685b30aeac78f0571063e3e60b8
ShivamBansal96 <bansal.shivam1216@gmail.com>
2024-01-26 02:23:31 +0530

    done

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let productsContainer = document.querySelector("#products");
+let cartContainer = document.querySelector("#cart");
+
+let numberOne = document.querySelector("#numberOne");
+let numberTwo = document.querySelector("#numberTwo");
+let numberThree = document.querySelector("#numberThree");
+
+
 let plusOne = document.querySelector("#increaseButtonOne");
 let plusTwo = document.querySelector("#increaseButtonTwo");
 let plusThree = document.querySelector("#increaseButtonThree");
@@ -8,19 +16,99 @@ let totalOne = document.querySelector("noOfProductsOne");
 let totalTwo = document.querySelector("noOfProductsTwo");
 let totalThree = document.querySelector("noOfProductsThree");
 
+
 function addOne() {
     let currentValueOne = Number(document.querySelector("#noOfProductsOne").innerText);
     document.querySelector("#noOfProductsOne").innerText = currentValueOne + 1; 
+
+
+
+    // making new divs for cart
+    let cartOne = document.createElement("div");
+    // let totalContainer = document.createElement("div");
+
+    // linking css with js
+    cartOne.classList.add("cartOne");
+    // totalContainer.classList.add("totalContainer");
+
+    cartContainer.append(cartOne);
+    // cartContainer.append(cartOne, totalContainer);
+    // totalContainer.appendChild(total);
+
+    let cartOnePara = document.createElement("p");
+    let cartOneCalculation = document.createElement("p");
+    let total = document.createElement("p");
+
+    // linking css with js
+    // cartOnePara.classList.add("cartOnePara");
+    
+    // cartOneCalculation.innerText = `${currentValueOne}`;
+    cartOneCalculation.innerText = "1*100";
+
+
+    cartOne.appendChild(cartOnePara, cartOneCalculation)
+
+    cartOnePara.innerText = "Sylvester Tshirt";
+
+
+    // () => {
+    //     numberOne * totalOne = result;
+    // }
+
+    
+
+
+
+    // console.log(cartOneCalculation);
+    console.log(numberOne);
+    
+
+    // console.log(cartContainer);
+    
+
+
+
+
 }
 
 function addTwo() {
     let currentValueTwo = Number(document.querySelector("#noOfProductsTwo").innerText);
     document.querySelector("#noOfProductsTwo").innerText = currentValueTwo + 1; 
+
+    let cartTwo = document.createElement("div");
+
+    cartTwo.classList.add("cartTwo");
+
+    let cartTwoPara = document.createElement("p");
+    let cartTwoCalculation = document.createElement("p");
+
+    cartTwoPara.innerText = "Harry Potter Leviosa";
+
+    cartContainer.append(cartTwo);
+    cartTwo.appendChild(cartTwoPara, cartTwoCalculation)
+
 }
 
 function addThree() {
     let currentValueThree = Number(document.querySelector("#noOfProductsThree").innerText);
     document.querySelector("#noOfProductsThree").innerText = currentValueThree + 1; 
+
+    
+    let cartThree = document.createElement("div");
+
+    cartThree.classList.add("cartThree");
+
+    let cartThreePara = document.createElement("p");
+    let cartThreeCalculation = document.createElement("p");
+
+    cartThreePara.innerText = "Hogwarts";
+    cartThreeCalculation.innerText = "1*300 = 300";
+
+    cartContainer.append(cartThree);
+    cartThree.appendChild(cartThreePara, cartThreeCalculation)
+
+
+
 }
 // function addThree() {
 //     let currentValueThree = Number(document.querySelector("#noOfProductsThree").innerText);
